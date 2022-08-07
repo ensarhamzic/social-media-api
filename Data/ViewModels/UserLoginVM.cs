359ebb6622b89d5e4b6f4d0462cc3f0bc56a7e18
Ensar <ensarhamzic01@gmail.com>
2022-08-07 22:18:08 +0200

    user login DTO

---
+++ Data/ViewModels/UserLoginVM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaAPI.Data.ViewModels
+{
+    public class UserLoginVM
+    {
+        [Required]
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
