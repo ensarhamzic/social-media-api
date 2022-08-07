bf214a3d1fa3b38a64faec54d00dd379daef578e
Ensar <ensarhamzic01@gmail.com>
2022-08-07 17:16:46 +0200

    added services

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+
+using Microsoft.EntityFrameworkCore;
+using SocialMediaAPI.Data;
+using SocialMediaAPI.Data.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +11,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(c =>
+{
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddTransient<UserService>();
 
 var app = builder.Build();
+
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
