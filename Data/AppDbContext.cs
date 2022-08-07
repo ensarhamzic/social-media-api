8d0818cb503179e5694850c714353e8054937305
Ensar <ensarhamzic01@gmail.com>
2022-08-07 17:17:02 +0200

    configured ef core

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaAPI.Data.Models;
+
+namespace SocialMediaAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        private IConfiguration configuration;
+
+        public AppDbContext(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnectionString"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasAlternateKey(u => u.Email)
+                .HasName("user_email_unique");
+
+            modelBuilder.Entity<User>()
+                .HasAlternateKey(u => u.Username)
+                .HasName("user_username_unique");
+
+            modelBuilder.Entity<Post>()
+                .HasOne<User>(p => p.User)
+                .WithMany(u => u.Posts)
+                .HasForeignKey(p => p.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Comment>()
+                .HasOne<User>(c => c.User)
+                .WithMany(u => u.Comments)
+                .HasForeignKey(c => c.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Like>()
+                .HasIndex(l => new { l.UserId, l.PostId }).IsUnique();
+
+            modelBuilder.Entity<Follow>()
+                .HasIndex(f => new { f.UserId, f.FollowingId }).IsUnique();
+
+            modelBuilder.Entity<Follow>()
+                .HasOne(f => f.User)
+                .WithMany(u => u.Following)
+                .HasForeignKey(f => f.UserId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Follow>()
+                .HasOne(f => f.Following)
+                .WithMany(u => u.Followers)
+                .HasForeignKey(f => f.FollowingId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Follow> Follows { get; set; }
+
+    }
+}
