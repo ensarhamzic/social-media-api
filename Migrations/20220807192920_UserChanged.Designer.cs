0bf4965ca8c0d1e85ab3cd141e126758db2302a6
Ensar <ensarhamzic01@gmail.com>
2022-08-07 22:17:28 +0200

    fields changed

--- Migrations/20220807192920_UserChanged.Designer.cs
+++ Migrations/20220807192920_UserChanged.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SocialMediaAPI.Data;
 
@@ -11,9 +12,10 @@ using SocialMediaAPI.Data;
 namespace SocialMediaAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220807192920_UserChanged")]
+    partial class UserChanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
