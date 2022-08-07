62854b1aa7e287fac524524abce1a3115c1c34d6
Ensar <ensarhamzic01@gmail.com>
2022-08-07 17:16:39 +0200

    initialized user service

---
+++ Data/Services/UserService.cs
@@ -0,0 +1,15 @@
+﻿using SocialMediaAPI.Data.Models;
+using SocialMediaAPI.Data.ViewModels;
+
+namespace SocialMediaAPI.Data.Services
+{
+    public class UserService
+    {
+        private AppDbContext dbContext;
+        public UserService(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+    }
+}
