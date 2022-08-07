7a8070e3c211be351b335d597be09a30a9b4efdb
Ensar <ensarhamzic01@gmail.com>
2022-08-07 17:16:56 +0200

    added register DTO

---
+++ Data/ViewModels/UserRegisterVM.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaAPI.Data.ViewModels
+{
+    public class UserRegisterVM
+    {
+        [Required]
+        public string FirstName { get; set; } = string.Empty;
+        [Required]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        public string Username { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        [MinLength(8)]
+        public string ConfirmPassword { get; set; } = string.Empty;
+        public string? PictureURL { get; set; }
+    }
+}
