e66dfa1e381bb3b2db514a5b240c7215378221ec
Ensar <ensarhamzic01@gmail.com>
2022-08-07 17:15:49 +0200

    initialized controller

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialMediaAPI.Data.Services;
+using SocialMediaAPI.Data.ViewModels;
+
+namespace SocialMediaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private UserService userService;
+        public UsersController(UserService userService)
+        {
+            this.userService = userService;
+        }
+
+    }
+}
