aa336c0c33118e463c0da001c750ffa0d8b8e265
Ensar <ensarhamzic01@gmail.com>
2022-08-08 17:07:29 +0200

    post view model

---
+++ Data/ViewModels/PostVM.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaAPI.Data.ViewModels
+{
+    public class PostVM
+    {
+        [Required]
+        public string Text { get; set; } = string.Empty;
+    }
+}
