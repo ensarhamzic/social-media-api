f9e0d7efd5f4c75879422646209204526a99d8f5
Ensar <ensarhamzic01@gmail.com>
2022-08-08 17:07:23 +0200

    add post

---
+++ Controllers/PostsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SocialMediaAPI.Data.Services;
+using SocialMediaAPI.Data.ViewModels;
+
+namespace SocialMediaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        private PostService postService;
+
+        public PostsController(PostService postService)
+        {
+            this.postService = postService;
+        }
+
+        [HttpPost, Authorize]
+        public IActionResult CreatePost([FromBody] PostVM request)
+        {
+            try
+            {
+                var newPost = postService.CreatePost(request);
+                return Created(nameof(newPost), newPost);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+    }
+}
