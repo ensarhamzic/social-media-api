42d2709eb9dce8d15e69349063d797d2f2f62dd0
Ensar <ensarhamzic01@gmail.com>
2022-08-08 21:09:16 +0200

    more methods

--- Data/Services/PostService.cs
+++ Data/Services/PostService.cs
@@ -19,11 +19,74 @@ namespace SocialMediaAPI.Data.Services
             var post = new Post()
             {
                 Text = request.Text,
-                UserId = int.Parse(httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.PrimarySid))
+                UserId = GetAuthUserId()
             };
             dbContext.Posts.Add(post);
             dbContext.SaveChanges();
             return post;
+        }
+
+        
+
+        public Post UpdatePost(string stringId, PostVM request)
+        {
+            string errorMessage = $"Post with id of {stringId} is not found!";
+            CheckId(stringId, out int id, out bool isValid);
+            if(isValid)
+            {
+                var foundPost = dbContext.Posts.FirstOrDefault(p => p.Id == id);
+                if(foundPost != null)
+                {
+                    if (UserHasPermission(foundPost.UserId))
+                    {
+                        foundPost.Text = request.Text;
+                        dbContext.SaveChanges();
+                        return foundPost;
+                    }
+                    else
+                        errorMessage = "You don't have permission to update this post!";
+                }
+            }
+            throw new Exception(errorMessage);
+        }
+
+        public string DeletePost(string stringId)
+        {
+            string errorMessage = $"Post with id of {stringId} is not found!";
+            CheckId(stringId, out int id, out bool isValid);
+            if (isValid)
+            {
+                var foundPost = dbContext.Posts.FirstOrDefault(p => p.Id == id);
+                if (foundPost != null)
+                {
+                    if (UserHasPermission(foundPost.UserId))
+                    {
+                        dbContext.Posts.Remove(foundPost);
+                        dbContext.SaveChanges();
+                        return "Post successfully deleted";
+                    }
+                    else
+                        errorMessage = "You don't have permission to delete this post!";
+                }
+            }
+            throw new Exception(errorMessage);
+        }
+
+        private void CheckId(string stringId, out int id, out bool isValid)
+        {
+            bool valid = int.TryParse(stringId, out int convertedId);
+            isValid = valid;
+            id = convertedId;
+        }
+
+        private bool UserHasPermission(int userId)
+        {
+            return userId == GetAuthUserId();
+        }
+
+        private int GetAuthUserId()
+        {
+            return int.Parse(httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.PrimarySid));
         }
     }
 }
