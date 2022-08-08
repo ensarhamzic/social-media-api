286502cc41d62752f98a6af5c98ee1cd67f2e5c7
Ensar <ensarhamzic01@gmail.com>
2022-08-08 23:01:59 +0200

    comment vm

---
+++ Data/ViewModels/CommentVM.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SocialMediaAPI.Data.ViewModels
+{
+    public class CommentVM
+    {
+        [Required]
+        public string Text { get; set; } = string.Empty;
+    }
+}
