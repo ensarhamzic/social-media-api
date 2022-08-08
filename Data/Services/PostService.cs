6646d373dac1b822b2c8b3999777326f80ca2d07
Ensar <ensarhamzic01@gmail.com>
2022-08-08 17:07:37 +0200

    post service

---
+++ Data/Services/PostService.cs
@@ -0,0 +1,29 @@
+﻿using SocialMediaAPI.Data.Models;
+using SocialMediaAPI.Data.ViewModels;
+using System.Security.Claims;
+
+namespace SocialMediaAPI.Data.Services
+{
+    public class PostService
+    {
+        private AppDbContext dbContext;
+        private IHttpContextAccessor httpContextAccessor;
+        public PostService(AppDbContext dbContext, IHttpContextAccessor httpContextAccessor)
+        {
+            this.dbContext = dbContext;
+            this.httpContextAccessor = httpContextAccessor;
+        }
+
+        public Post CreatePost(PostVM request)
+        {
+            var post = new Post()
+            {
+                Text = request.Text,
+                UserId = int.Parse(httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.PrimarySid))
+            };
+            dbContext.Posts.Add(post);
+            dbContext.SaveChanges();
+            return post;
+        }
+    }
+}
