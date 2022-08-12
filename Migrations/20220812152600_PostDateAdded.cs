36b68158d97aad5a71db860061d922de0b91a10b
Ensar <ensarhamzic01@gmail.com>
2022-08-12 17:49:36 +0200

    date added

---
+++ Migrations/20220812152600_PostDateAdded.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMediaAPI.Migrations
+{
+    public partial class PostDateAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Date",
+                table: "Posts",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Posts");
+        }
+    }
+}
