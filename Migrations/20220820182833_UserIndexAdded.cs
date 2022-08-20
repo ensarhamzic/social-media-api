c9270cc0fef7a7c40025d61dda9b52f70bbb726e
Ensar <ensarhamzic01@gmail.com>
2022-08-20 21:15:04 +0200

    changed unique

---
+++ Migrations/20220820182833_UserIndexAdded.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMediaAPI.Migrations
+{
+    public partial class UserIndexAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropUniqueConstraint(
+                name: "user_email_unique",
+                table: "Users");
+
+            migrationBuilder.DropUniqueConstraint(
+                name: "user_username_unique",
+                table: "Users");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Username",
+                table: "Users",
+                column: "Username",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Username",
+                table: "Users");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "user_email_unique",
+                table: "Users",
+                column: "Email");
+
+            migrationBuilder.AddUniqueConstraint(
+                name: "user_username_unique",
+                table: "Users",
+                column: "Username");
+        }
+    }
+}
